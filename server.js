d91340d25510e9ce78fd03a5803222af3be914af
Yamina21 <yaminaguenez@gmail.com>
2021-06-30 11:48:22 +0100

    update

--- server.js
+++ server.js
@@ -18,21 +18,20 @@ const posts = [
 }   
 
 ]
-app.get('/posts', authenticateToken, (req, res)=>{
-    
-    res.json(posts.filter(posts => posts.username === req.user.name))
+app.get('/posts', authenticateToken, (req, res) => {
+  res.json(posts.filter(post => post.username === req.user.name))
 })
 
 app.post('/login', (req, res)=> {
     //Authenticate User
-  const username = req.params.username
+  const username = req.body.username
    const user = {name : username}
   const accessToken = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET)
   res.json({accessToken: accessToken})
 })
 
 function authenticateToken(req, res, next) {
-    const authHeader = req.headers["authorization"] 
+    const authHeader = req.headers.authorization 
     const token = authHeader && authHeader.split(' ')[1]
     if (token == null) return res.sendStatus(401)
   
